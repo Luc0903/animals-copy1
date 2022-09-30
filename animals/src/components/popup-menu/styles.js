358d7a7356288c4a98aed233c3646707dcc48ba1
Lucio Vasquez <castrocociolucio@gmail.com>
2022-09-30 16:48:01 -0300

    Creando el componente del PopupMenu

---
+++ animals/src/components/popup-menu/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const PopUpMenuContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #3d8361;
+  height: 90px;
+  width: 190px;
+  border-radius: 6px;
+  color: #eef2e6;
+`;
+
+export const LinkStyles = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-inline: 4px;
+`;
